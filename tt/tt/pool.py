a137facb5b47a1875987ec7892de720511fe667c
Om Sisodia <99199126+omsisodia@users.noreply.github.com>
2023-04-26 09:04:20 +0530

    Delete tt directory

--- tt/tt/pool.py
+++
@@ -1,6 +0,0 @@
-import pymysql as mysql
-
-def ConnectionPool():
-    dbe =mysql.connect(host = "localhost", port = 3306, user = "root", password = "Rakesh@1968", db = "timetable")
-    cmd = dbe.cursor()
-    return (dbe, cmd)
